d10cf92acdca26e73be4811b502657b70c556f04
VishalChoudhary <vishalghn953@gmail.com>
2023-02-03 20:14:09 +0530

    noteList/note dynamic commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import NotesList from './components/NotesList';
 
 const App = () =>{
+
+  const [notes,setNotes]=useState([
+    {
+    id : nanoid(),
+    text : "This is my first note",
+    date : "15/08/2021"
+    },
+    {
+    id : nanoid(),
+    text : "This is my second note",
+    date : "16/08/2021"
+    },
+    {
+    id : nanoid(),
+    text : "This is my third note",
+    date : "17/08/2021"
+    }
+  ]);
+
   return (
     <div className='container'>
-      <NotesList />
+      <NotesList notes={notes}/>
     </div>
   )
 }
