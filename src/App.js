43d2bccfbd928d81cde4b82f8a895d6f3708f264
VishalChoudhary <vishalghn953@gmail.com>
2023-02-04 03:49:39 +0530

    local-storage save&load commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 import Search from "./components/Search";
@@ -8,24 +8,41 @@ const App = () => {
   const [notes, setNotes] = useState([
     {
       id: nanoid(),
-      text: "This is my first note",
-      date: "15/08/2021",
+      text: "This is my first note!",
+      date: "15/04/2021",
     },
     {
       id: nanoid(),
-      text: "This is my second note",
-      date: "16/08/2021",
+      text: "This is my second note!",
+      date: "21/04/2021",
     },
     {
       id: nanoid(),
-      text: "This is my third note",
-      date: "17/08/2021",
+      text: "This is my third note!",
+      date: "28/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my new note!",
+      date: "30/04/2021",
     },
   ]);
 
   const [searchText, setSearchText] = useState("");
 
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
+
+    if (savedNotes) {
+      setNotes(savedNotes);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
+  }, [notes]);
 
   const addNote = (text) => {
     const date = new Date();
